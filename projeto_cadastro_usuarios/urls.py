866e20bd7156bdb82be16678c696a4670385a266
DanonePlayer <ce6519@gmail.com>
2023-07-12 15:20:08 -0500

    teste

--- projeto_cadastro_usuarios/urls.py
+++ projeto_cadastro_usuarios/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# HTTP REQUEST <- HTTP RESPONSE
+
+# HTTP REQUEST
+
+def myview(request):
+    return HttpResponse("sas")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("sobre/", myview)
 ]
