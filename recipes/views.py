8127dd8247909234966affeb3c243c6dd0b94e62
DanonePlayer <ce6519@gmail.com>
2023-07-14 15:19:53 -0500

    criação de dois apps, e paginação recipes

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "recipes/home.html", context={"name": "recipe carlos",})
+    return render(request, "recipes/pages/home.html", context={"name": "recipe carlos",})
