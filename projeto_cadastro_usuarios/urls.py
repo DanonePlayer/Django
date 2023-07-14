37b6d08f9762634b5ed61bcfc4ce46cd2fa45196
DanonePlayer <ce6519@gmail.com>
2023-07-14 13:53:13 -0500

    Outra app

--- projeto_cadastro_usuarios/urls.py
+++ projeto_cadastro_usuarios/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("app_cadastro_usuarios.urls"))
+    path("", include("app_cadastro_usuarios.urls")),
+    path("recipes", include("recipes.urls")),
 ]
---
+++ recipes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from recipes.views import home
+
+# HTTP REQUEST <- HTTP RESPONSE
+
+# HTTP REQUEST
+
+
+
+urlpatterns = [
+    path("", home),
+]
