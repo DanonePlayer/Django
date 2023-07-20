72adbb734bffbc2eea6427b543471d89c4b724e9
DanonePlayer <ce6519@gmail.com>
2023-07-20 12:09:11 -0500

    aplicando herança. blocos, id, for e etc.. nas pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+from Base_codes.Utils.recipes.factory import make_recipe
 # Create your views here.
 
 
 def home(request):
-    return render(request, "recipes/pages/home.html", context={"name": "recipe carlos",})
+    return render(request, "recipes/pages/home.html", context={"recipes": [make_recipe() for _ in range(10)],})
 
 def recipe(request, id):
-    return render(request, "recipes/pages/recipe-view.html", context={"name": "recipe carlos",})
+    return render(request, "recipes/pages/recipe-view.html", context={"recipe": make_recipe(), "is_detail_page": True})
