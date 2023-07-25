895b55d05d20e38d9d362972f098f81a1f358df8
DanonePlayer <ce6519@gmail.com>
2023-07-25 17:57:51 -0500

    arrumando algumas coisas
    usando shotcut get_list_or_404
    e mostrando esse erro na pagina caso não encontre a categoria no banco de dados

--- recipes/urls.py
+++ recipes/urls.py
@@ -13,6 +13,6 @@ app_name = 'recipes'
 
 urlpatterns = [
     path("/", views.home, name="home"),
-    path("/receitas/category/<int:category_id>/", views.category, name="category"),
+    path("/category/<int:category_id>/", views.category, name="category"),
     path("/receitas/<int:id>/", views.recipe, name="recipe"),
 ]
