6bb829be5164a962e9758b1dc5320707c1c82517
DanonePlayer <ce6519@gmail.com>
2023-08-11 16:35:40 -0500

    salvando registro no BD
    Modificações de algumas validações e as 3 formas de fazer validação, e até que fim salvando na base de dados

--- app_cadastro_usuarios/forms.py
+++ app_cadastro_usuarios/forms.py
@@ -1,7 +1,19 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+# def strong_password(password):
+#     regex = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*{8,}$")
+
+#     if not regex.match(password):
+#         raise ValidationError((
+#             "password must have at least one uppercase letter,"
+#             "one lowercase letter and one number. The length shoud be at least 8 characters."
+#         ),
+#             code="Invalid"
+#         )
 
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(
@@ -13,8 +25,8 @@ class RegisterForm(forms.ModelForm):
             "required": "A senha não deve estar vazia"
         },
         help_text=(
-            "password must have at least one uppercase letter,"
-            "one lowercase letter and one number. The length shoud be at least 8 characters."
+            "A senha deve ter pelo menos uma letra maiúscula, uma letra minúscula e um número."
+            "O comprimento deve ser de pelo menos 8 caracteres."
         )
     )
 
@@ -62,19 +74,47 @@ class RegisterForm(forms.ModelForm):
             }),
         }
     
-    def clean_password(self):#validaespecifico do campo
-        data = self.cleaned_data.get("password")
+    # def clean_password(self):#validaespecifico do campo
+    #     data = self.cleaned_data.get("password")
 
-        if"atencion" in data:
-            raise ValidationError(
-                "DIGITO ERRADO",
-                code="invalid",
+    #     if"atencion" in data:
+    #         raise ValidationError(
+    #             "DIGITO ERRADO",
+    #             code="invalid",
                 
-            )
+    #         )
 
-        print(data)
+    #     print(data)
 
-        return data
+    #     return data
     
     def clean(self):#valida todo o formulario
         cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+        password2 = cleaned_data.get("password2")
+        first_name = cleaned_data.get("first_name")
+        username = cleaned_data.get("username")
+
+        if first_name in password:
+            raise ValidationError({
+                "password": "Sua senha não pode ser igual ao seu nome",
+            })
+
+        if password != password2:
+            raise ValidationError({
+                "password": "Erro, suas senhas não coicidem",
+                "password2": "Erro, suas senhas não coicidem"
+
+            })
+        
+        if not regex.match(password):
+            raise ValidationError({
+                "password": "Sua senha contem menos de 8 caracteres"
+            })
+        
+        if not regex.match(username):
+            raise ValidationError({
+                "username": "Seu user contem menos de 8 caracteres"
+            })
+
--- app_cadastro_usuarios/views.py
+++ app_cadastro_usuarios/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -20,5 +21,12 @@ def register_create(request):
     POST = request.POST
     request.session["register_form_data"] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        data = form.save(commit=False)
+        print(data)
+        form.save()
+        messages.success(request, "PARABENS OLOROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+        del(request.session["register_form_data"])
 
     return redirect("cadastro:login")
